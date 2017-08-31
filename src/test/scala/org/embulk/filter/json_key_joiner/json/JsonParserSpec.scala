5824fc0c134d0b8ed05857f48f0b0442a29808be
masatoshi.shimada <smdmts@gmail.com>
2017-08-31 15:04:09 +0900

    add test

--- src/test/scala/org/embulk/filter/json_key_joiner/json/JsonParserSpec.scala
+++ src/test/scala/org/embulk/filter/json_key_joiner/json/JsonParserSpec.scala
@@ -1,5 +1,21 @@
 package org.embulk.filter.json_key_joiner.json
 
-class JsonParserSpec {
+import org.scalatest.Matchers
+
+class JsonParserSpec extends org.scalatest.FlatSpec with Matchers {
+
+  it should "be parse" in {
+    val appender = Map("abc" -> "efg")
+    val before = """
+                   |{
+                   |  "name" : "John",
+                   |  "age" : 50
+                   |}
+                 """.stripMargin
+    val after = JsonParser(before, appender).noSpaces
+
+    after shouldBe """{"name":"John","age":50,"abc":"efg"}"""
+
+  }
 
 }
